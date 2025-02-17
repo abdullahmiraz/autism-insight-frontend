0098ad59d6857d29c74afc27d0c5587ca9e8a893
abdullahmiraz <abdullahmiraz2015@gmail.com>
2025-02-17 14:11:32 +0600

    auth route added, build fine

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,12 +1,39 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import TabNavigation from "./TabNavigation";
+import { useRouter } from "next/navigation";
+import { useAuth } from "../../lib/useAuth";
 
 const Main = () => {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to '/login' if the user is not logged in
+    if (!loading && !user) {
+      alert(
+        "Only logged-in users can access this page. Create an account/ login."
+      );
+      router.push("/login");
+    }
+  }, [user, loading, router]);
+
+  if (loading) {
+    return <div>Loading...</div>; // Show loading state while checking auth
+  }
+
   return (
     <div>
       <div className="max-w-3xl mx-auto bg-white p-6 shadow-md rounded-lg">
         <h1 className="text-2xl font-bold text-center mb-4">Autism Insight</h1>
-        <TabNavigation />
+        {!user ? (
+          <p className="text-center">
+            Only logged-in users can access this page. Create an account/ login
+          </p>
+        ) : (
+          <TabNavigation />
+        )}
       </div>
     </div>
   );
